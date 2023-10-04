1c01b3311c0860e399a0f6ae6620a1e9493995eb
mrfritzg <mrfritz@gamil.com>
2023-10-03 20:21:32 -0400

    added email

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ export default function About(props) {
         <div>
             <h1>The Super Heros/Villian Website</h1>
      <h2>This Website provides Heros/Villian from mutliple universes. While most Websites give you only once comic book Universe,  we give you Marvel, DC and more!.</h2>
-          <h3>Please email us at  SuperHerosVillian@aol.com for any sugeestions, questions or comments</h3>
+          <h3>Please email us at  octelnet@gmail.com for any sugeestions, questions or comments</h3>
         </div>
     )
 }
